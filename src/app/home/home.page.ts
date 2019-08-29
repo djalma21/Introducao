e4a2e1285429452b32f7a8aaec654546eb4b45f2
djalma21 <djalma21cruz@gmail.com>
2019-08-29 11:29:57 -0300

    primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  nome: string = "Djalma";
+  cor = "primary";
+
+  humor = "sad";
+
+  checked_humor = false;
+
+
+
   constructor() {}
+
+  trocarCor():void{
+    console.log('Chamou trocarCor!');
+    if (this.cor == "primary") {
+    this.cor = "danger";
+    }
+    else if (this.cor == "danger") {
+    this.cor = "success";
+    }
+    else if (this.cor == "success") {
+    this.cor = "warning";
+    } else{
+    this.cor = "primary";
+  }
+}
+  mudaHumor(): void{
+    console.log('Chamou mudaHumor!');
+    this.checked_humor = !this.checked_humor;
+    if (this.checked_humor == true){
+      this.humor = "happy";
+    }else{
+      this.humor = "sad"
+    }
+  }
 
 }
